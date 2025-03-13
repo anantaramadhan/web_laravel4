1a20fce7a3fad8b6fef9e0ca33e92e7f6917904c
ants <anantaramadhan00@gmail.com>
2025-03-13 13:55:43 +0700

    Initial commit

---
+++ dispora/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\Mailer\Transport\RoundRobinTransport;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+//acara3
+Route::get('foo',function(){
+    return 'Hello world';
+});
+
+Route::get('user/{id}', function($id){
+    return 'User'.$id;
+});
+
+Route::get('post/{post}/comments/{comment}', function($postId , $commentId){
+    //
+});
+
+Route::get('/user', [UserController::class, 'index']);
